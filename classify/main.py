0acbf155d2114ef5ed49ea9cc3ef97051af61444
Paul Lindner <lindner@inuus.com>
2024-11-26 05:06:50 +0000

    add timeout middleware and try to use less gpu memory

--- classify/main.py
+++ classify/main.py
@@ -1,16 +1,19 @@
-from fastapi import FastAPI, File, UploadFile, Query
-from fastapi.responses import PlainTextResponse
+import asyncio
+import time
+
+from fastapi import FastAPI, File, UploadFile, Query, Request, Response
+from fastapi.responses import JSONResponse
 from PIL import Image
 from io import BytesIO
 import torch
 from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor
 import clip
 from clip import load
+from starlette.status import HTTP_504_GATEWAY_TIMEOUT
 
 # Load the CLIP model
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = load("ViT-B/32", device=device)
-
 
 app = FastAPI()
 
@@ -20,6 +23,21 @@ transform = Compose([
     CenterCrop(224),
     ToTensor()
 ])
+
+REQUEST_TIMEOUT_ERROR = 5  # Threshold
+
+# Adding a middleware returning a 504 error if the request processing time is above a certain threshold
+@app.middleware("http")
+async def timeout_middleware(request: Request, call_next):
+    try:
+        start_time = time.time()
+        return await asyncio.wait_for(call_next(request), timeout=REQUEST_TIMEOUT_ERROR)
+
+    except asyncio.TimeoutError:
+        process_time = time.time() - start_time
+        return JSONResponse({'detail': 'Request processing time excedeed limit',
+                             'processing_time': process_time},
+                            status_code=HTTP_504_GATEWAY_TIMEOUT)
 
 @app.post('/classify_image/')
 def classify_image(category: list[str] = Query(1), upload: UploadFile = File(...)):
@@ -44,6 +62,8 @@ def classify_image(category: list[str] = Query(1), upload: UploadFile = File(...
         
     # Compute the raw similarity score
     similarity = (100.0 * image_features @ text_features.T)
+    del image_features, text_features, text, image
+
     similarity_softmax = similarity.softmax(dim=-1)
     
     # Define a threshold

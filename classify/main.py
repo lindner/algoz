2471e4deafc9fac4d76c4fd12abf663efd3057eb
Paul Lindner <lindner@inuus.com>
2024-11-26 17:41:25 +0000

    Add tighter timeouts and a GPU OOM handler

--- classify/main.py
+++ classify/main.py
@@ -9,7 +9,7 @@ import torch
 from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor
 import clip
 from clip import load
-from starlette.status import HTTP_504_GATEWAY_TIMEOUT
+from starlette.status import HTTP_504_GATEWAY_TIMEOUT, HTTP_429_TOO_MANY_REQUESTS
 
 # Load the CLIP model
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -24,7 +24,7 @@ transform = Compose([
     ToTensor()
 ])
 
-REQUEST_TIMEOUT_ERROR = 5  # Threshold
+REQUEST_TIMEOUT_ERROR = 2  # Threshold
 
 # Adding a middleware returning a 504 error if the request processing time is above a certain threshold
 @app.middleware("http")
@@ -42,27 +42,38 @@ async def timeout_middleware(request: Request, call_next):
 @app.post('/classify_image/')
 def classify_image(category: list[str] = Query(1), upload: UploadFile = File(...)):
     # processes categories as a query param
-    raw = upload.file.read()  # this is dangerous for big files
-    image = Image.open(BytesIO(raw)).convert('RGB')
-    
-    # Preprocess the image
-    image = transform(image)
-    image = image.unsqueeze(0).to(device)
     categories = category
-    
     print("categories: ", categories)
 
-    # Prepare the text inputs
-    text = torch.cat([clip.tokenize(f"a photo of a {c}") for c in categories]).to(device)
+    raw = upload.file.read()  # this is dangerous for big files
+    try:
+      image = Image.open(BytesIO(raw)).convert('RGB')
     
-    # Compute the features and compare the image to the text inputs
-    with torch.no_grad():
-        image_features = model.encode_image(image)
-        text_features = model.encode_text(text)
+      # Preprocess the image
+      image = transform(image)
+      image = image.unsqueeze(0).to(device)
+
+      # Prepare the text inputs
+      text = torch.cat([clip.tokenize(f"a photo of a {c}") for c in categories]).to(device)
+    
+      # Compute the features and compare the image to the text inputs
+      with torch.no_grad():
+          image_features = model.encode_image(image)
+          text_features = model.encode_text(text)
         
-    # Compute the raw similarity score
-    similarity = (100.0 * image_features @ text_features.T)
-    del image_features, text_features, text, image
+      # Compute the raw similarity score
+      similarity = (100.0 * image_features @ text_features.T)
+
+      # try to reduce gpu memory
+      del image_features, text_features, text, image
+    except RuntimeError as e:
+      if 'out of memory' in str(e):
+        print("CUDA out of memory..")
+        return JSONResponse({'detail': 'Failed to allocate GPU memory'},
+                            status_code=HTTP_429_TOO_MANY_REQUESTS)
+        # Retry your training code with reduced batch size
+      else:
+        raise e 
 
     similarity_softmax = similarity.softmax(dim=-1)
     
